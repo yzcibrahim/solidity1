40b4da354ab053ef50c81162e9b58c024207f258
"yzc" <"yzcibrahim@gmail.com">
2022-01-11 00:48:52 +0300

    ico

--- contracts/yzctoken.sol
+++ contracts/yzctoken.sol
@@ -41,7 +41,7 @@ contract MyCryptos is ERC20Interface{
         return balances[tokenOwner];
     }
 
-    function transfer(address to, uint tokens) public override returns(bool success){
+    function transfer(address to, uint tokens) public virtual override returns(bool success){
         require(balances[msg.sender]>=tokens);
 
         balances[to] +=tokens;
@@ -64,7 +64,7 @@ contract MyCryptos is ERC20Interface{
             return true;
     }
 
-     function transferFrom(address from, address to, uint tokens) public override returns (bool){
+     function transferFrom(address from, address to, uint tokens) public virtual override returns (bool){
          require(allowed[from][to] >= tokens);
          require(balances[from] >= tokens);
 
@@ -75,4 +75,98 @@ contract MyCryptos is ERC20Interface{
          return true;
      }
 
+}
+
+contract YzcIco is MyCryptos{
+    address public admin;
+    address payable public deposit;
+    uint tokenPrice = 0.001 ether; // 1Eth =1000 crpt2; 1crpt2 = 0.001ETH;
+    uint public hardCap = 300 ether;
+    uint public raisedAmount;
+    uint public saleStart = block.timestamp;
+    uint public saleEnd = block.timestamp + 604800;//ends in 1 week
+    uint public tokenTradeStart = saleEnd + 604800;//transferable in a week after sale end
+    uint public maxInvestment = 5 ether;
+    uint public minInvestment = 0.1 ether;
+
+    enum State {beforeStart, running, afterEnd, halted}
+    State public icoState;
+
+    constructor(address payable _deposit){
+        deposit = _deposit;
+        admin = msg.sender;
+        icoState = State.beforeStart;
+    }
+
+    modifier onlyAdmin(){
+        require(msg.sender == admin);
+        _;
+    }
+
+    function halt() public onlyAdmin{
+        icoState = State.halted;
+    }
+
+    function resume() public onlyAdmin{
+        icoState = State.running;
+    }
+
+    function changeDepositAddress( address payable newDeposit) public onlyAdmin{
+        deposit = newDeposit;
+    }
+
+    function getCurrentState() public view returns(State){
+        if(icoState == State.halted){
+        return State.halted;
+        }else if(block.timestamp < saleStart){
+            return State.beforeStart;
+        }else if(block.timestamp >= saleStart && block.timestamp <= saleEnd){
+            return State.running;
+        }else{
+            return State.afterEnd;
+        }
+    }
+
+    event Invest(address investor, uint value, uint tokens);
+
+    function invest() payable public returns(bool){
+        icoState = getCurrentState();
+        require(icoState == State.running,"state is not running");
+        
+        require(msg.value >= minInvestment && msg.value <= maxInvestment,"value is not appropirate");
+        raisedAmount += msg.value;
+        require(raisedAmount <= hardCap,"hardcap reached");
+
+        uint tokens = msg.value/tokenPrice;
+
+        balances[msg.sender] += tokens;
+        balances[founder] -= tokens;
+        deposit.transfer(msg.value);
+
+        emit Invest(msg.sender, msg.value, tokens);
+
+        return true;
+    }
+
+    receive() payable external{
+        invest();
+    }
+
+    function transfer(address to, uint tokens) public override returns(bool success){
+        require(block.timestamp > tokenTradeStart);
+        return super.transfer(to, tokens); //MyCryptos.transfer(to, tokens);
+        }
+
+     function transferFrom(address from, address to, uint tokens) public override returns (bool){
+         require(block.timestamp > tokenTradeStart);
+         MyCryptos.transferFrom(from, to, tokens);
+         return true;
+         }
+
+         function burn() public returns(bool){
+            icoState = getCurrentState();
+            require(icoState == State.afterEnd);
+            balances[founder] = 0;
+            return true;
+         }
 }
